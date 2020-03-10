ff4542e3e1a8eeb7c8122de7f35c33df123eb874
Eibhinn Lee <g00226502@gmit.ie>
2020-03-10 21:49:51 +0000

    Added BMI.py

---
+++ BMI.py
@@ -0,0 +1,15 @@
+# Eibhinn Lee
+# Calculating the BMI 
+# weight in KG
+# Height in CM
+
+weight = 65
+height = 180
+
+# BMI = kg/metre sq
+# BMI = x
+
+x = 65/(1.8 * 1.8)
+
+print("BMI of a person 65kg and 1.8metres tall is",x)
+
