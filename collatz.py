467a9558faf26c252b365823c8c4af0f9347a812
Eibhinn Lee <g00226502@gmit.ie>
2020-04-01 21:13:01 +0100

    explained program

--- collatz.py
+++ collatz.py
@@ -3,16 +3,18 @@
 # output (if even) = divide by 2
 # output (if odd) = integer * 3 + 1
 
-x=int(input("Enter a positive integer here:"))
+x = int(input("Enter a positive integer here:"))
 
+# End program when x =1
 while x > 0:
     if x ==1:
         print(x)
         break
-
+# if x is even(no rounding) divide by 2
     if x % 2 == 0:
         print(x)
         x = x / 2
+# if x is odd (rounding) multiply by 3 and add 1        
     elif x % 2 != 0:
         print(x)
         x = x*3+1  
