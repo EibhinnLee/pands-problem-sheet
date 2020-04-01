d090f715bca540d82aaeebf0e10ab8db6ab63ec1
Eibhinn Lee <g00226502@gmit.ie>
2020-04-01 20:53:34 +0100

    explained process

--- secondstring.py
+++ secondstring.py
@@ -1,5 +1,8 @@
 # Eibhinn Lee
+# Sentence s = string
+# Goal = print every second letter backwards of the string
 
 s = "The quick brown fox jumps over the lazy dog."
 
+# print string in reverse choosing every letter
 print(s[::-2])
