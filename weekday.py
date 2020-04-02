1294c3fa49861d93c0d12ad40f0fbc2726eca091
Eibhinn Lee <g00226502@gmit.ie>
2020-04-02 19:48:13 +0100

    Clean up program

--- weekday.py
+++ weekday.py
@@ -12,10 +12,12 @@ daysofweek ={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5
 
 print("Today is", daysofweek[day])
 
+# Based on dictionary above(daysofweek), any number up to and including 4 = weekday
 if day <= 4:
-    print(" its a weekday ")
+    print(" Its a weekday ")
+# if not, weekend    
 else:
-    print("its the weekend")
+    print("Its the weekend")
 
 
 
