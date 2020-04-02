34d178aa524350e03795d0f9d42c1bb8dff356d7
Eibhinn Lee <g00226502@gmit.ie>
2020-04-02 21:09:31 +0100

    newtons method

--- squareroot.py
+++ squareroot.py
@@ -1,11 +1,20 @@
 # Eibhinn Lee
 # Function for squareroot
+# sq root of 14.5
+# variables
+n = 14.5
+m = int(input("Enter number of guesses: "))
+approx = (1/2)*n
 
-#Jonathan Harper
-def sqrt (v):
-    ans= v**(0.5)
-    return ans
+# for loop in range between 0 & no. of guesses
+for i in range (0,m):
+    # Ist loop : better = (0.5) * (7.25+14.5/7.25)
+    better=(1/2) * (approx+n/approx)
+    # new value for approx, loop continues until approx = better
+    approx = better
+    print(approx)
 
-v = float(input("enter value to be rooted:"))
-print(sqrt(v))
+
+# the more the answer repeats itself after running program = more accurate
+
 
